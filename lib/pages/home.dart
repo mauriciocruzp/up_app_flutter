70484f9d54f77c9bad4b09945ba9f64072323403
Mauricio Cruz <mawcp7@gmail.com>
2024-02-27 02:13:17 -0600

    Refactor UI layout in home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -20,97 +20,106 @@ class _HomeState extends State<Home> {
           child: Column(
             children: [
               Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Calendario Académico 2023-2024",
-                          style: TextStyle(
-                              color: Color(0xff0D0C0C),
-                              fontSize: 15,
-                              fontWeight: FontWeight.w600,
-                              fontFamily: "Inter")),
-                    ],
-                  )),
+                margin: const EdgeInsets.only(top: 10),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Calendario Académico 2023-2024",
+                        style: TextStyle(
+                            color: Color(0xff0D0C0C),
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: "Inter")),
+                  ],
+                ),
+              ),
               Container(
-                  color: const Color(0xffd9d9d9),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset('assets/images/Calendario_2023_2024.jpg',
-                          width: 280, height: 300),
-                    ],
-                  )),
+                color: const Color(0xffd9d9d9),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/images/Calendario_2023_2024.jpg',
+                        width: 280, height: 300),
+                  ],
+                ),
+              ),
               Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Encuentra el sitio que buscas",
-                          style: TextStyle(
-                              color: Color(0xff0D0C0C),
-                              fontSize: 15,
-                              fontWeight: FontWeight.w600,
-                              fontFamily: "Inter")),
-                    ],
-                  )),
+                margin: const EdgeInsets.only(top: 10),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Encuentra el sitio que buscas",
+                        style: TextStyle(
+                            color: Color(0xff0D0C0C),
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: "Inter")),
+                  ],
+                ),
+              ),
               Container(
-                  color: const Color(0xffd9d9d9),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset('assets/images/mapa.jpg',
-                          width: 230, height: 170),
-                    ],
-                  )),
+                color: const Color(0xffd9d9d9),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/images/mapa.jpg',
+                        width: 230, height: 170),
+                  ],
+                ),
+              ),
               Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Eventos",
-                          style: TextStyle(
-                              color: Color(0xff0D0C0C),
-                              fontSize: 15,
-                              fontWeight: FontWeight.w600,
-                              fontFamily: "Inter")),
-                    ],
-                  )),
+                margin: const EdgeInsets.only(top: 10),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Eventos",
+                        style: TextStyle(
+                            color: Color(0xff0D0C0C),
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: "Inter")),
+                  ],
+                ),
+              ),
               Container(
                 color: const Color(0xffd9d9d9),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Column(children: [
-                      Image.asset('assets/images/evento1.jpg',
-                          width: 130, height: 120),
-                      ButtonBar(
-                        alignment: MainAxisAlignment.center,
-                        children: [
-                          TextButton(
-                            onPressed: () {
-                              // Perform some action
-                            },
-                            child: const Text('Ver más'),
-                          ),
-                        ],
-                      ),
-                    ]),
-                    Column(children: [
-                      Image.asset('assets/images/evento2.jpg',
-                          width: 120, height: 130),
-                      ButtonBar(
-                        alignment: MainAxisAlignment.center,
-                        children: [
-                          TextButton(
-                            onPressed: () {
-                              // Perform some action
-                            },
-                            child: const Text('Ver más'),
-                          ),
-                        ],
-                      ),
-                    ]),
+                    Column(
+                      children: [
+                        Image.asset('assets/images/evento1.jpg',
+                            width: 130, height: 120),
+                        ButtonBar(
+                          alignment: MainAxisAlignment.center,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                // Perform some action
+                              },
+                              child: const Text('Ver más'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Image.asset('assets/images/evento2.jpg',
+                            width: 120, height: 130),
+                        ButtonBar(
+                          alignment: MainAxisAlignment.center,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                // Perform some action
+                              },
+                              child: const Text('Ver más'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
