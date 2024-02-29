8fe4b9ea5d545224f0f4be5b30e5d1343ce1ad60
Mauricio Cruz <mawcp7@gmail.com>
2024-02-29 03:45:03 -0600

    Remove unused imports and clean up code

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:upch_events_app/pages/calendar_page.dart';
-import 'package:upch_events_app/pages/event_details_page.dart';
-import 'package:upch_events_app/pages/events_page.dart';
-import 'package:upch_events_app/pages/home_page.dart';
-import 'package:upch_events_app/pages/main_page.dart';
-import 'package:upch_events_app/pages/map_page.dart';
 import 'package:upch_events_app/pages/start_page.dart';
 
 void main() {
@@ -23,7 +17,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-
       home: const StartPage(),
     );
   }
