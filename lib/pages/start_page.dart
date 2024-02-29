9d28851cd2514847ab82d65279341ac2154e3113
Mauricio Cruz <mawcp7@gmail.com>
2024-02-29 01:14:53 -0600

    Add StartPage class to handle the initial page of the app

---
+++ lib/pages/start_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'main_page.dart';
+
+class StartPage extends StatelessWidget {
+  const StartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color(0xff8b00be),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/logoup.png', width: 200),
+              IconButton(
+                icon: const Icon(Icons.arrow_circle_right_outlined,
+                    size: 60, color: Colors.white,),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const MainPage()));
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
